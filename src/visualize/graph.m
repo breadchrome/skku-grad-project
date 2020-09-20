b68f1ed20e3952fe496347705700d587fa92cb9e
Junhyun Kim <me@junbread.win>
2020-09-20 19:40:16 +0900

    Add graph script

---
+++ src/visualize/graph.m
@@ -0,0 +1,13 @@
+x = []
+y = []
+z = []
+s = []
+for i = 0:29
+    T = csvread(sprintf("%s.a0.s%d.csv", argv(){1}, i))
+    x = [x; T(:,1)]
+    y = [y; T(:,2)]
+    z = [z; T(:,3)]
+    s = [s; ones(size(T)(1,1), 1) * i]
+end
+scatter3(x, y, z, 10, s, "filled")
+input("Paused...")
